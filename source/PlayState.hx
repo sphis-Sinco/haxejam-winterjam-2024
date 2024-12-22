3aa5ac0e18a8b88a16a5ac18a1a75633a7a12e37
IdealistCat <mattallan.h@gmail.com>
2024-12-22 13:47:17 -0600

    fish

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,16 +9,30 @@ class PlayState extends FlxState
 {
 	var bg:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.CYAN);
 
+	var player:FlxSprite = new FlxSprite(0, 0).loadGraphic('assets/images/fish.png', true, 64, 64);
+
 	override public function create()
 	{
 		FlxG.camera.fade(FlxColor.BLACK, 3.25, true);
 		add(bg);
+
+		player.animation.add('idle', [0]);
+		player.animation.add('eat', [1, 1, 2, 2, 2, 3, 3, 0], 24, false);
+		player.animation.play('idle');
+		player.screenCenter();
+		add(player);
 
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		if (player.animation.finished && player.animation.name != 'idle')
+			player.animation.play('idle');
+
+		if (FlxG.keys.justReleased.SPACE)
+			player.animation.play('eat');
+
 		super.update(elapsed);
 	}
 }
